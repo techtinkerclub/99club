52a3e239a22d555d2657cdad3d45dce098b08d1e
techtinkerclub <techtinkerclub@gmail.com>
2026-09-18 16:23:52 +0100

    Add analytics configuration scaffold

---
+++ assets/99club/analytics-config.js
@@ -0,0 +1,8 @@
+/* 99 Club Studio analytics configuration.
+ * Add the GA4 Measurement ID here when the Google Analytics web stream exists.
+ * Example format: G-ABC123DE45
+ */
+window.TT99_ANALYTICS_CONFIG = Object.freeze({
+  ga4MeasurementId: '',
+  privacyPath: '/privacy/'
+});
