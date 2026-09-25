a12d4a87a49587c4bb4b5e01c937f0958ab31f7a
techtinkerclub <techtinkerclub@gmail.com>
2026-09-25 20:45:41 +0100

    Match Goodies production script order in browser QA

--- scripts/99club/goodies-browser-qa.js
+++ scripts/99club/goodies-browser-qa.js
@@ -22,8 +22,8 @@ if(mode==='prepare'){
 <script src="/assets/99club/goodies-core.js"></script>
 <script src="/assets/99club/goodies-interaction.js"></script>
 <script src="/assets/99club/goodies-challenge.js"></script>
-<script src="/assets/99club/goodies-number-line-v6.js"></script>
 <script src="/assets/99club/goodies-tools-a.js"></script>
+<script src="/assets/99club/goodies-number-line-v6.js"></script>
 <script src="/assets/99club/goodies-tools-b.js"></script>
 <script>
 (function(){
